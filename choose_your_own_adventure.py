b583ba3ada570793a795d08bb3da2e76a15142a1
Roy Contreras Benitez <roy7474@gmail.com>
2023-07-12 19:57:09 -0400

    finish the options for questions. fixed bug

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,7 +1,7 @@
 name = input('Type your name: ')
 print("Welcome", name, "to this adventure!")
 
-answer = input('You are on a dirt road, it has come to and end and you can go left or right. Which way would you like to go? ').lower
+answer = input('You are on a dirt road, it has come to and end and you can go left or right. Which way would you like to go? ').lower()
 
 if answer == "left":
     answer = input('you come to a river, you can walk around it or swem across? Type walk around or swim to cross')
@@ -13,9 +13,24 @@ if answer == "left":
 
     else:
         print('Not a valid option. You lose.')
-        
+
 elif answer == 'right':
-    print()
+    answer = input('you come to a bridge, it look wobbly, do you wan to cross it or head back? (cross/back)')
+
+    if answer == 'back':
+        print('You go back and lose')
+    elif answer == 'cross':
+        answer = input(' You cross the bridge and talk to a stranger. Do you talk to them ? (yes/no)')
+
+        if answer == 'yes':
+            print('You talk to the stranger and they give you gold. You win')
+        elif answer == 'no':
+            print('You ignore them. They get offended and you lose.')
+
+        else:
+            print('Not a valid option. You lose.')
+    else:
+        print('Not a valid option. You lose.')
 
 else:
     print('Not a valid option. You lose')
